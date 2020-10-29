8eb29772da92f40d90117868c0c4f4bfe5b73c12
MANALCH <manalch@oocl.com>
2020-10-29 22:01:29 +0800

    [spring-todo-api][joseph] test: add integration test for create todos

--- src/test/java/com/oocl/todolistapi/services/integration/TodoServiceIntegrationTest.java
+++ src/test/java/com/oocl/todolistapi/services/integration/TodoServiceIntegrationTest.java
@@ -7,9 +7,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -38,5 +40,22 @@ class TodoServiceIntegrationTest {
                 .andExpect(jsonPath("$[0].id").isNumber())
                 .andExpect(jsonPath("$[0].text").value("Remember to do TDD first"))
                 .andExpect(jsonPath("$[0].done").value(false));
+    }
+
+    @Test
+    void should_create_todo_when_creating_a_todo_given_a_new_object() throws Exception {
+        String todoJson = "{\n" +
+                "    \"text\":\"do tdd first\",\n" +
+                "    \"done\":false\n" +
+                "}";
+
+        mockMvc.perform(post("/todos")
+                .content(todoJson)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").isNumber())
+                .andExpect(jsonPath("$.text").value("do tdd first"))
+                .andExpect(jsonPath("$.done").value(false));
     }
 }
